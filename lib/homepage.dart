85628e169689fbf9215cc4806c085f0787c1bf62
MrNewaz <saifur.saif@northsouth.edu>
2021-10-26 02:24:17 +0600

    init

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,7 +9,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final TextEditingController nameController = TextEditingController();
-  String name = '';
+  final TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -19,36 +20,81 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              name,
-              style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value == '') {
+                    return 'This cant be empty';
+                  }
+                  return null;
+                },
+                controller: nameController,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  labelText: 'Enter Name',
+                  hintText: 'Saif Newaz',
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            TextField(
-              onChanged: (value) => setState(() {
-                name = value;
-              }),
-              controller: nameController,
-              decoration: const InputDecoration(
-                labelText: 'Enter Name',
-                hintText: 'Saif Newaz',
-                border: OutlineInputBorder(),
+              SizedBox(height: 15),
+              TextFormField(
+                validator: (value) {
+                  if (value!.length < 6) {
+                    return 'This cant be empty';
+                  }
+                  return null;
+                },
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.password),
+                  labelText: 'Enter Password',
+                  hintText: '123456',
+                  border: OutlineInputBorder(),
+                ),
+                // keyboardType: TextInputType.multiline,
+                // maxLines: null,
+                obscureText: true,
               ),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Print'),
-              ),
-            )
-          ],
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() == true) {
+                      _formKey.currentState?.save();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Form Submitted'),
+                          backgroundColor: Colors.green,
+                        ),
+                      );
+                      // showDialog(
+                      //   context: context,
+                      //   builder: (context) => AlertDialog(
+                      //     title: const Text('Thank You'),
+                      //     content: const Text('Sumbitted Successfully'),
+                      //     actions: [
+                      //       ElevatedButton(
+                      //           onPressed: () {
+                      //             Navigator.pop(context);
+                      //           },
+                      //           child: const Text('ok'))
+                      //     ],
+                      //     shape: RoundedRectangleBorder(
+                      //       borderRadius: BorderRadius.circular(16),
+                      //     ),
+                      //   ),
+                      // );
+                    }
+                  },
+                  child: const Text('Print'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
